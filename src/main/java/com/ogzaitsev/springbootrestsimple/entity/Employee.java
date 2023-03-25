6a224322e6edfb451079e49ce3c9547d0055e6f8
ogzaitsev <m18027242@edi.misis.ru>
2023-03-25 17:44:15 +0300

    Employee entity

---
+++ src/main/java/com/ogzaitsev/springbootrestsimple/entity/Employee.java
@@ -0,0 +1,62 @@
+package com.ogzaitsev.springbootrestsimple.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "salary")
+    private int salary;
+    @Column
+    private String department;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}

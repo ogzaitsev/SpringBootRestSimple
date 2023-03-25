bca1b7eac00a2f9f60ded19831ef35910a5f99ab
ogzaitsev <m18027242@edi.misis.ru>
2023-03-25 15:03:14 +0300

    Init commit

---
+++ src/test/java/com/ogzaitsev/springbootrestsimple/SpringBootRestSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogzaitsev.springbootrestsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
